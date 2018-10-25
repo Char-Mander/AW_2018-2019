d2b6cc4825c0dbc3df69fd99fd683f467750665f
Char-Mander <36884991+Char-Mander@users.noreply.github.com>
2018-10-25 17:39:02 +0200

    Práctica guiada 2

--- Practicas/Práctica guiada/Practica_guiada.js
+++ Practicas/Práctica guiada/Practica_guiada.js
@@ -9,7 +9,7 @@ let listaTareas=[
 
 
 
-
+/*Doevuelve un array con los textos de aquellas tareas que estén sin terminar*/
 function getToDoTasks(tasks){
     let resultado=[];
     let done= n => !Object.keys(n)==="done" || n["done"]===true;
@@ -17,4 +17,37 @@ function getToDoTasks(tasks){
     resultado=tasks.filter(n => !done(n));
 
     return resultado;
+}
+
+/*Devuelve un array con las tareas de "tasks" que contengan en su
+lista de etiquetas, la etiqueta "tag".*/
+function findByTag(tasks, tag){
+    let list = tasks.filter(t => t.tags.some(n => n == tag));
+
+    return list;
+}
+
+
+/*Devuelve un array con las tareas del array "tasks"
+ que contengan al menos una etiqueta que coincida con una de las
+ del array "tags".*/
+function findByTags(tasks, tags){
+    let list = tasks.filter(n => n.tags.some(t => tags.some(m => m == t)));
+
+    return list;
+}
+
+/*Crea una tarea con el texto que se le pasa por parámetro*/
+function createTask(texto){
+    let task = {
+        texto: "",
+        tags: []
+    };
+
+    let array = texto.split(" ");
+    task.texto=array.filter(n=>n[0]!== "@").join(" ");
+    task.tags=array.filter(n=>n[0]=== "@");
+
+    return task;
+
 }
