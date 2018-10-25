9f771137a96b57ff6cbb12dd6cda61034bcb45d6
Char-Mander <36884991+Char-Mander@users.noreply.github.com>
2018-10-25 16:36:02 +0200

    primer ejercicio de la práctica hecho

---
+++ Practicas/Práctica guiada/Practica_guiada.js
@@ -0,0 +1,22 @@
+"use strict";
+
+let listaTareas=[
+                {text: "Preparar práctica AW", tags: ["AW", "practica"]},
+                {text: "Mirar fechas congreso", done:true, tags: []},
+                {text: "Ir al supermercado", tags: ["personal"]},
+                {text: "Mudanza", done: false, tags: ["personal"]}
+];
+
+
+
+
+function getToDoTasks(tasks){
+    let resultado=[];
+    let done= n => !Object.keys(n)==="done" || n["done"]===true;
+
+    resultado=tasks.filter(n => !done(n));
+
+    return resultado;
+}
+
+console.log(getToDoTasks(listaTareas));
