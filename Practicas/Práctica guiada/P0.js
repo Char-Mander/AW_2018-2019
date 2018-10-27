b458ef405851ab51747ea96a17d863d65c381fdc
Char-Mander <laurajf29@gmail.com>
2018-10-27 19:29:28 +0200

    Update Practicas/Práctica guiada/P0.js

--- Practicas/Práctica guiada/P0.js
+++ Practicas/Práctica guiada/P0.js
@@ -64,4 +64,4 @@ function createTask(texto){
     return task;
 }
 
-console.log(createTask("ir  al  gym    @per  @salud  por    la      tarde"));
+console.log(createTask("ir       al          gym          @per  @salud           por    la      tarde"));
