2f06802ac6f55b49783998609594bb2aaea0444d
Char-Mander <36884991+Char-Mander@users.noreply.github.com>
2018-11-08 17:13:45 +0100

    función isUserCorrect realizada

---
+++ Practicas/Práctica guiada 3/DAOUsers.js
@@ -0,0 +1,52 @@
+"use strict";
+
+const mysql = require('mysql');
+/*
+const pool = mysql.createPool({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "gestor_tareas"
+}); 
+*/
+class DAOUsers{
+    constructor(pool) {
+        this.pool=pool;
+    }
+
+    isUserCorrect(email, password, cb_isUserCorrect){
+        this.pool.getConnection(function(err, connection){
+            if(err){
+                console.log(`Error de conexión a la base de datos`);
+            }
+            else{
+                const sql = `SELECT * FROM user WHERE ? = email AND ? = password`;
+                connection.query(sq, [email, password], function(err, resultado){
+                    connection.release();
+                    if(err){
+                        cb_isUserCorrect(err, false);
+                    } else if(resultado==null){
+                        cb_isUserCorrect(null, false);
+                    }
+                    else{
+                        cb_isUserCorrect(null, true);
+                    }
+                })
+            }
+        });
+    }
+
+    getUserImageName(email, cb_getUserImageName){
+        this.pool.getConnection(function(err, connection){
+            if(err){
+                console.log(`Error de conexión a la base de datos`);
+            }
+            else{
+
+            }
+        });
+    }
+    
+}
+
+module.exports = DAOUsers;
