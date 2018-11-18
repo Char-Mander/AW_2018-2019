9c030269d250d6bacf9efd07b79aee3058e105a8
Char-Mander <laurajf29@gmail.com>
2018-11-18 11:12:24 +0100

    Modificado el css y html para que coja formularios

--- Practicas/Práctica guiada 4/app.js
+++ Practicas/Práctica guiada 4/app.js
@@ -20,6 +20,7 @@ const daoT = new DAOTasks(pool);
 
 //  Ficheros estáticos
 const ficherosEstaticos =path.join(__dirname, "public");
+
 app.use(express.static(ficherosEstaticos));
 
 //  Listado de tareas
@@ -31,6 +32,28 @@ app.get("/tasks", function(request, response){
         else{
             response.status(200);
             response.render("tasks", {taskList: tareas}); 
+        }
+    });
+   
+});
+
+app.use(bodyParser.urlencoded({ extended : true }));
+
+//  Añadir la tarea a la lista de tareas
+app.post("/addTask", function(request, response){
+    //se añade la tarea a la base de datos  
+
+    let cuerpo = [];
+    cuerpo.push(request.body.Tarea_añadida);
+    var task = sacarTarea(cuerpo);
+
+    daoT.insertTask("usuario@ucm.es", function(error, task){
+        if(error){
+            response.status(500);
+        }
+        else{
+            response.status(200);
+            response.redirect("/tasks");
         }
     });
    
@@ -74,3 +97,20 @@ app.listen(config.port, function(err) {
        console.log(`Servidor arrancado en el puerto ${config.port}`);
    }
 });
+
+
+//Sirve para parsear las tareas que se añaden
+function sacarTarea(cuerpo){
+    let tarea=[{
+        id: "",
+        texto: "",
+        done: 0,
+        tags: [] 
+    }];
+    cuerpo[0].split(" ");
+    tarea.texto = cuerpo[0].filter(n => !n.startsWith("@") && n !== "").join(" ").trim();
+    tarea.tags = cuerpo[0].filter(n => n.startsWith("@"));
+    tarea.tags = tarea.tags.map(t => t.slice(1, t.length));
+
+    return tarea;
+}
