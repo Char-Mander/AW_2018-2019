3630f4cf468f3b23ac3dc89439031104985598d3
mvicbar <mariavba@ucm.es>
2018-12-13 20:19:57 +0100

    Chorrada de que se cambien las imágenes lmao

---
+++ Practicas/Práctica guiada 6/index.js
@@ -0,0 +1,47 @@
+"use strict";
+
+function crearTablero() {
+    let modo = $("input[name='modo']:checked").val();
+
+    if (modo === "Fácil") {
+        alert("Modo de juego: " + modo);
+        for (let i = 1; i <= 12; i++) {
+            let carta = $(`<li class="carta ${i}">
+            <div class="imagen"> 
+                <img src="./imgs/unicornio.png">
+            <div>
+        </li>`);
+            $("#lista_cartas").append(carta);
+        }
+        //12 cartas
+    } else if (modo === "Medio") {
+        //24 cartas
+        for (let i = 0; i < 24; i++) {
+            let carta = $(`<li>Carta ${i}</li>`);
+            $("#lista_cartas").append(carta);
+        }
+
+    } else if (modo === "Difícil") {
+        //36 cartas
+        for (let i = 0; i < 36; i++) {
+            let carta = $(`<li>Carta ${i}</li>`);
+            $("#lista_cartas").append(carta);
+        }
+
+    } else {
+        alert("No se ha seleccionado modo de juego");
+    }
+}
+
+function voltear(){
+
+}
+
+$(function () {
+    $("#boton_inicio").on("click", crearTablero);
+
+    $("#lista_cartas").on("click", "li", (function () {
+        $("img", this).attr("src", "./imgs/cupcake.png");
+        //alert($(this).html());
+    }))
+})
