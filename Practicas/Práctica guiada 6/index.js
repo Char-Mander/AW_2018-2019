9541c1c8e010c77318b3da547a633c2221844182
mvicbar <mariavba@ucm.es>
2018-12-16 14:13:29 +0100

    Volteado de cartas tras un tiempo

--- Practicas/Práctica guiada 6/index.js
+++ Practicas/Práctica guiada 6/index.js
@@ -9,11 +9,11 @@ function crearTablero() {
     if (modo === "Fácil") {
         //12 cartas
         $("ul").css("grid-template-columns", "15vw 15vw 15vw 15vw 15vw 15vw").css("grid-template-rows", "15vw 15vw");
-        
+
         for (let i = 1; i <= 12; i++) {
             let carta = $(`<li class="carta ${i}">
             <div class="imagen"> 
-                <div class="front"><img src="./imgs.cupcake.png"></div>
+                <div class="front"><img src="./imgs/cupcake.png"></div>
                 <div class="back"><img src="./imgs/unicornio.png"></div>
             <div>
             </li>`);
@@ -21,17 +21,17 @@ function crearTablero() {
         }
 
         $("img").css("width", "12vw")
-                .css("height", "12vw");
+            .css("height", "12vw");
 
     } else if (modo === "Medio") {
         //24 cartas
         $("ul").css("grid-template-columns", "12vw 12vw 12vw 12vw 12vw 12vw 12vw 12vw")
-                .css("grid-template-rows", "12vw 12vw 12vw");
-        
+            .css("grid-template-rows", "12vw 12vw 12vw");
+
         for (let i = 1; i <= 24; i++) {
             let carta = $(`<li class="carta ${i}">
             <div class="imagen"> 
-            <div class="front"><img src="./imgs.cupcake.png"></div>
+            <div class="front"><img src="./imgs/cupcake.png"></div>
             <div class="back"><img src="./imgs/unicornio.png"></div>
             <div>
             </li>`);
@@ -39,17 +39,17 @@ function crearTablero() {
         }
 
         $("img").css("width", "10vw")
-                .css("height", "10vw");
+            .css("height", "10vw");
 
     } else if (modo === "Difícil") {
         //36 cartas
         $("ul").css("grid-template-columns", "9vw 9vw 9vw 9vw 9vw 9vw 9vw 9vw 9vw")
-                .css("grid-template-rows", "9vw 9vw 9vw 9vw");
-        
+            .css("grid-template-rows", "9vw 9vw 9vw 9vw");
+
         for (let i = 1; i <= 36; i++) {
             let carta = $(`<li class="carta ${i}">
             <div class="imagen"> 
-                <div class="front"><img src="./imgs.cupcake.png"></div>
+                <div class="front"><img src="./imgs/cupcake.png"></div>
                 <div class="back"><img src="./imgs/unicornio.png"></div>
             <div>
             </li>`);
@@ -57,7 +57,7 @@ function crearTablero() {
         }
 
         $("img").css("width", "7vw")
-                .css("height", "7vw");
+            .css("height", "7vw");
 
     } else {
         alert("No se ha seleccionado modo de juego");
@@ -67,27 +67,41 @@ function crearTablero() {
     $("div.front").hide();
 }
 
+function voltear() {
+    let $front =$(this).find(".front");
+    let $back = $(this).find(".back");
 
+    $front.show();
+    $back.hide();
+
+    setTimeout(function () {
+        voltear2($front, $back);
+    }, 1000);
+}
+
+function voltear2(front, back) {
+    front.hide();
+    back.show();
+}
+
+function sumarClick(){
+}
 
 $(function () {
 
     $("#boton_inicio").on("click", crearTablero);
 
-    $("#lista_cartas").on("click", "li", (function () {
-        //let frontElems = document.getElementsByClassName("front");
-        //if(frontElems.length<2){
-            $("img", this).attr("src", "./imgs/cupcake.png");
-            //$("div.front", this).show();
-            //$("div.back", this).hide();
-            /*if(frontElems.length==1){
-             setTimeout(voltear, 4000);
-            }*/
-            //alert($(this).html());
-        //}
-    }))
+    $("#lista_cartas").on("click", "li", voltear);
+    $("#lista_cartas").on("click", "li", sumarClick);
+
 })
 
-function voltear() {
-     $("div.front").hide();
-     $("div.back").show();
-}
+$("#div").click(function () { // evento click
+    var $t = $(this);
+    $t.hide();
+    setTimeout(function () {
+        $t.show();
+    }, 3000); /// tiempo en milisegundos
+});
+
+
