cf7a4c8fa33e31c3ccd18f3536e8124732aa46e8
mvicbar <mariavba@ucm.es>
2018-12-20 12:25:57 +0100

    Sólo se pueden levantar dos cartas

--- Practicas/Práctica guiada 6/index.js
+++ Practicas/Práctica guiada 6/index.js
@@ -1,8 +1,7 @@
 "use strict";
 
-let cartas = [];
 let carta = undefined;
-let segunda = false;
+let segunda = false, levantar_mas_cartas = true;
 let cartasUsadas;
 
 //  Crea el tablero del juego en base al modo de juego que elija el usuario
@@ -16,7 +15,6 @@ function crearTablero() {
     //Reiniciamos el contador
 
     $(".num_clicks").find(".num").text(0);
-    $(".num_clicks").find(".consultar_clicks").text(0);
 
     if (modo === "Fácil") {
         //12 cartas
@@ -25,15 +23,15 @@ function crearTablero() {
 
         for (let i = 1; i <= 12; i++) {
             let n = Math.floor(Math.random() * (13 - 1)) + 1;
-            if(cartasUsadas.some(elem => elem == n)){
-                while(cartasUsadas.some(elem => elem == n)){
+            if (cartasUsadas.some(elem => elem == n)) {
+                while (cartasUsadas.some(elem => elem == n)) {
                     n = Math.floor(Math.random() * (13 - 1)) + 1;
                 }
             }
-           
+
             let cartaPar = n;
-            if(n%2==0){
-                n=n-1;
+            if (n % 2 == 0) {
+                n = n - 1;
             }
 
             let carta = $(`<li class="carta${i}">
@@ -60,15 +58,15 @@ function crearTablero() {
 
         for (let i = 1; i <= 24; i++) {
             let n = Math.floor(Math.random() * (25 - 1)) + 1;
-            if(cartasUsadas.some(elem => elem == n)){
-                while(cartasUsadas.some(elem => elem == n)){
+            if (cartasUsadas.some(elem => elem == n)) {
+                while (cartasUsadas.some(elem => elem == n)) {
                     n = Math.floor(Math.random() * (25 - 1)) + 1;
                 }
             }
 
             let cartaPar = n;
-            if(n%2==0){
-                n=n-1;
+            if (n % 2 == 0) {
+                n = n - 1;
             }
 
             let carta = $(`<li class="carta${i}">
@@ -95,18 +93,18 @@ function crearTablero() {
 
         for (let i = 1; i <= 36; i++) {
             let n = Math.floor(Math.random() * (37 - 1)) + 1;
-            if(cartasUsadas.some(elem => elem == n)){
-                while(cartasUsadas.some(elem => elem == n)){
+            if (cartasUsadas.some(elem => elem == n)) {
+                while (cartasUsadas.some(elem => elem == n)) {
                     n = Math.floor(Math.random() * (37 - 1)) + 1;
                 }
             }
 
             let cartaPar = n;
-            if(n%2==0){
-                n=n-1;
+            if (n % 2 == 0) {
+                n = n - 1;
             }
 
-            
+
             let carta = $(`<li class="carta${i}">
             <div class="imagen"> 
                 <div class="front"><img src="./imgs/${n}.png"></div>
@@ -133,55 +131,92 @@ function crearTablero() {
     $(".front").hide();
 }
 
-//  Da la vuelta a las cartas, y las devuelve a su posición inicial tras un segundo
-/*function voltear() {
-    let num = parseInt($(".num_clicks").find(".consultar_clicks").text(), 10);
-    console.log("Número de clicks: " + num);
-    if (num <= 2) {
+
+//  Le da la vuelta a la carta siempre que no haya dos cartas levantadas
+function voltear() {
+    if (levantar_mas_cartas) {
         let $front = $(this).find(".front");
         let $back = $(this).find(".back");
         $front.show();
         $back.hide();
-
-        if (num == 2) {
-            setTimeout(function () {
-                voltearCarta();
-            }, 2000);
-        }
     }
-}*/
 
-function voltear() {
-    let $front = $(this).find(".front");
-    let $back = $(this).find(".back");
-    $front.show();
-    $back.hide();
+    if (segunda)
+        levantar_mas_cartas = false;
 }
 
-function voltearCarta() {
-    $(".num_clicks").find(".consultar_clicks").text(0);
-    $("div.front").hide();
-    $("div.back").show();
-}
 
 //  Suma 1 al número de clicks del usuario
 function sumarClick() {
     let num = parseInt($(".num_clicks").find(".num").text(), 10);
-    let num_comprobar_clicks = parseInt($(".num_clicks").find(".consultar_clicks").text(), 10);
-    //let carta = cartas.shift();
+    
+    if (levantar_mas_cartas) {
 
-    if (num === 0) {
-        num_comprobar_clicks = num_comprobar_clicks + 1;
-        num = num + 1;
+        if (num === 0)
+            num = num + 1;
+        else if ($(this).attr('class') !== carta.clase || carta === undefined)
+            num = num + 1;
 
-    } else if ($(this).attr('class') !== carta.clase || carta === undefined) {
-        num_comprobar_clicks = num_comprobar_clicks + 1;
-        num = num + 1;
     }
 
-    //cartas.push(carta);
     $(".num_clicks").find(".num").text(num);
-    $(".num_clicks").find(".consultar_clicks").text(num_comprobar_clicks);
+}
+
+
+//  Llama a la función que evalúa si las cartas son iguales pasado medio segundo
+function quitar() {
+    if (segunda) {
+        let $otra = $(this);
+        setTimeout(function () {
+            quitarCarta($otra)
+        }, 500)
+    }
+}
+
+
+//  Evalúa si las dos cartas levantadas son iguales y si lo son las quita del tablero, si no, las vuelve a dar la vuelta
+function quitarCarta($otra) {
+
+    if (segunda) {  //se han levantado dos cartas
+
+        if (carta.img === $otra.find(".front").find("img").attr('src')) {   //si ambas imágenes coinciden
+
+            let imagen_adivinada = $(`<div class="imagen_adivinada">
+            <img src="${carta.img}">
+            </div>`);
+            $("#imagenes_adivinadas").append(imagen_adivinada);
+
+            $(`.${carta.clase}`).css("visibility", "hidden");
+            $(`.${$otra.attr('class')}`).css("visibility", "hidden");
+
+        } else {    //si las imágenes son distintas
+
+            $otra.find(".front").hide();
+            $otra.find(".back").show();
+
+            carta.this.find(".front").hide();
+            carta.this.find(".back").show();
+
+        }
+
+        carta = undefined;
+        segunda = false;
+        levantar_mas_cartas = true;
+    }
+}
+
+
+//  Guarda en una variable la primera carta que se ha levantado, o bien indica que ya se han levantado dos cartas
+function guardarCarta() {
+
+    if (carta === undefined) {  //no se había clickado en ninguna carta antes
+        carta = {};
+        carta.img = $(this).find(".front").find("img").attr("src");
+        carta.clase = $(this).attr('class');
+        carta.this = $(this);
+    } else if ($(this).attr('class') !== carta.clase)   //se ha clickado en dos cartas
+        segunda = true;
+
 }
 
 
@@ -193,111 +228,3 @@ $(function () {
     $("#lista_cartas").on("click", "li", voltear);
     $("#lista_cartas").on("click", "li", quitar);
 })
-
-function quitar() {
-    if (segunda) {
-        console.log("this en quitar: " + $(this));
-        let $otra = $(this);
-        setTimeout(function () {
-            console.log("this en setimeout: " + $(this));
-            quitarCarta($otra)
-        }, 2000)
-    }
-}
-
-function quitarCarta($otra) {
-    console.log("Carta en quitarCarta: " + carta);
-    console.log($otra.attr('class'));
-
-    if (segunda) {
-
-        if (carta !== undefined && $otra.attr('class') !== carta.clase) {
-            console.log(carta.img);
-            console.log($otra.find(".front").find("img").attr('src'));
-            if (carta.img === $otra.find(".front").find("img").attr('src')) {
-                console.log("Imagenes iguales");
-                let imagen_adivinada = $(`<div class="imagen_adivinada">
-            <img src="${carta.img}">
-            </div>`);
-                $("#imagenes_adivinadas").append(imagen_adivinada);
-
-                $(`.${carta.clase}`).css("visibility", "hidden");
-                $(`.${$otra.attr('class')}`).css("visibility", "hidden");
-            } else {
-                console.log("Imágenes distintas");
-
-                /*let $front = $otra.find(".front");
-                let $back = $otra.find(".back");
-                $front.hide();
-                $back.show();*/
-
-                $otra.find(".front").hide();
-                $otra.find(".back").show();
-
-                let $front2 = carta.this.find(".front");
-                let $back2 = carta.this.find(".back");
-                $front2.hide();
-                $back2.show();
-            }
-        }
-
-        carta = undefined;
-        segunda = false;
-    }
-}
-
-/*function guardarCarta() {
-    let $carta = {};
-
-    $carta.img = $(this).find(".front").find("img").attr("src");
-    $carta.clase = $(this).attr('class');
-
-    if (cartas.length > 0) {
-        let $otra = cartas.shift();
-
-        if ($otra.clase === $carta.clase)
-            cartas.push($otra);
-        else {
-            cartas.push($otra);
-            cartas.push($carta);
-        }
-    } else {
-        cartas.push($carta);
-    }
-
-    console.log("Cartas después de guardarCarta: ");
-    for (let i = 0; i < cartas.length; ++i)
-        console.log(`${cartas[i].clase}`);
-}*/
-
-function guardarCarta() {
-    console.log("Carta clickada: " + carta);
-    if (carta === undefined) {
-        carta = {};
-        carta.img = $(this).find(".front").find("img").attr("src");
-        carta.clase = $(this).attr('class');
-        carta.this = $(this);
-    } else if ($(this).attr('class') !== carta.clase)
-        segunda = true;
-
-}
-
-/*function quitarCarta() {
-    let $carta1, $carta2;
-    if (cartas.length >= 2) {
-        $carta1 = cartas.shift();
-        $carta2 = cartas.shift();
-
-        if ($carta1.img === $carta2.img && $carta1.clase !== $carta2.clase) {
-            //Se muestra la imagen adivinada arriba a la derecha
-            let imagen_adivinada = $(`<div class="imagen_adivinada">
-            <img src="${$carta1.img}">
-            </div>`);
-            $("#imagenes_adivinadas").append(imagen_adivinada);
-
-            $(`.${$carta1.clase}`).css("visibility", "hidden");
-            $(`.${$carta2.clase}`).css("visibility", "hidden");
-        }
-
-    }
-}*/
