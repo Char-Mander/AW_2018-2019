9658407e099d604b5ce46fe07cc7d984577797d0
Char-Mander <laurajf29@gmail.com>
2018-12-24 18:58:06 +0100

    Añadido el config con la configuración del puerto. Comentado lo de la bd

--- Practicas/Práctica guiada 7/app.js
+++ Practicas/Práctica guiada 7/app.js
@@ -26,6 +26,7 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const config = require("./config");
 
 app.express();
 app.use(bodyParser.json());
@@ -54,16 +55,16 @@ let tasks = [
 let idCounter = 5;
 
 
-/*
+
 // Arrancar el servidor
-app.listen(3000, function(err) {
+app.listen(config.port, function(err) {
     if (err) {
         console.log("ERROR al iniciar el servidor");
     }
     else {
-        console.log(`Servidor arrancado en el puerto 3000`);
+        console.log(`Servidor arrancado en el puerto ${config.port}`);
     }
- });*/
+ });
 
  //Si no se pone nada, redirige a /tasks
 app.get("/", function(request, response){
