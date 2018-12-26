8e679dd18243cfb96fa67e34be1b0621874ce2bb
mvicbar <mariavba@ucm.es>
2018-12-26 13:56:05 +0100

    No funciona nada :cry:

--- Practicas/Práctica guiada 7/index.js
+++ Practicas/Práctica guiada 7/index.js
@@ -1,22 +1,111 @@
 "use strict";
 
+// loadTasks(); Solicita al servidor la lista de tareas (mediante una petición AJAX) e inserta cada una de ellas en el DOM para que se 
+// visualicen en la página.
+function loadTasks() {
+    $.ajax({
+        method: "GET",
+        url: "http://localhost:3000/tasks",
+
+        success: function (data, textStatus, jqXHR) {
+            data.forEach((task) => {
+                let tarea = taskToDOMElement(task);
+                $('#tareas').append(tarea);
+            })
+        },
+
+        error: function (jqXHR, testStatus, errorThrown) {
+            alert("Se ha producido un error: " + errorThrown);
+        }
+    });
+}
+
+
 // taskToDOMElement(task); A partir de un objeto con dos atributos (id y text) representando la información de una tarea, 
-// construye un elemento <x> con el contenido de dicha tarea y devuelve una selección al mismo. Este elemento contiene, además,
-// un atributo personalizado con el identificador de la tarea. En esta descripción, <x> representa el elemento HTML que se haya 
-// utilizado para representar cada tarea en el fichero tasks.html.
+// construye un elemento <x> con el contenido de dicha tarea y devuelve una selección al mismo. Este elemento contiene, 
+// además, un atributo personalizado con el identificador de la tarea. En esta descripción, <x> representa el elemento
+// HTML que se haya utilizado para representar cada tarea en el fichero tasks.html.
+function taskToDOMElement(task) {
+    let x = '<li id="tarea_' + task.id + '" > <div class="st"><div class="nombre"><h3>' + task.text + '</h3> </div> <div class="marcar" id="botón"> <button class="button" id="botón_eliminar_tarea"> Eliminar </button> </div> </div> </li>';
+    $("#tarea_" + task.id + "").data("id_tarea", task.id);
+
+    return x;
+}
+
+
+
+
+// onAddButtonClick(event) Maneja los eventos de clic en el botón [Añadir]. Debe obtener la cadena introducida en el 
+// cuadro de texto situado a la izquierda de dicho botón. Si esta cadena es no vacía, realizará una petición AJAX al 
+// servidor para insertar la tarea en la lista de tareas. Además, debe añadir el elemento del DOM correspondiente.
+function onAddButtonClick(event){
+
+    let id_tarea = $(event.target).data("id_tarea");
+
+    $.ajax({
+        method: "DELETE",
+        url: "/tasks/" + id_tarea,
+
+        success: function (data) {
+            //  Hay que añadir el elemento al DOM
+            loadTasks();
+        },
+
+        error: function (jqXHR, testStatus, errorThrown) {
+            alert("Se ha producido un error: " + errorThrown);
+        }
+    });
+
+    event.preventDefault();
+}
+
 
 // Manejador de inicialización del DOM, que se limita a llamar a la función loadTasks() y a asignar sendos manejadores de eventos a 
 // los botones [Eliminar] y al botón [Añadir] de la página. 
+$(function () {
 
-// loadTasks(); Solicita al servidor la lista de tareas (mediante una petición AJAX) e inserta cada una de ellas en el DOM para que se 
-// visualicen en la página.
+    loadTasks();
+    $("#botón").on("click", "button", onRemoveButtonClick);
+    $("#botón_añadir_tarea").on("click", onAddButtonClick);
 
-// onRemoveButtonClick(event) Maneja los eventos de clic en un botón [Eliminar]. Debe enviar una petición AJAX para eliminar la tarea 
-// correspondiente del servidor y eliminar dicha tarea del DOM.
+})
 
-// onAddButtonClick(event) Maneja los eventos de clic en el botón [Añadir]. Debe obtener la cadena introducida en el cuadro de texto 
-// situado a la izquierda de dicho botón. Si esta cadena es no vacía, realizará una petición AJAX al servidor para insertar la tarea 
-// en la lista de tareas. Además, debe añadir el elemento del DOM correspondiente.
+// onRemoveButtonClick(event) Maneja los eventos de clic en un botón [Eliminar]. Debe enviar una petición AJAX para 
+// eliminar la tarea correspondiente del servidor y eliminar dicha tarea del DOM.
+function onRemoveButtonClick(event){
+    let id_tarea = $(event.target).data("id_tarea");
 
+    $.ajax({
+        method: "DELETE",
+        url: "/tasks/" + id_tarea,
+
+        success: function (data) {
+            //  Hay que borrar el elemento del DOM
+            loadTasks();
+        },
+
+        error: function (jqXHR, testStatus, errorThrown) {
+            alert("Se ha producido un error: " + errorThrown);
+        }
+    });
+
+    event.preventDefault();
+}
+
+
+function createTask(texto){
+    let task = {
+        text: "",
+        tags: [],
+        done: 0
+    };
+
+    let array = texto.split(" ");
+    task.text = array.filter(n => !n.startsWith("@") && n !== "").join(" ").trim();
+    task.tags = array.filter(n => n.startsWith("@"));
+    task.tags = task.tags.map(t => t.slice(1, t.length));
+
+    return task;
+}
 
 
